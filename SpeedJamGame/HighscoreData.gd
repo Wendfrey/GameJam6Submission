71283b8bc7d0399a62a66c0318ba7958a619de9d
Wendfrey <toni.cano.redondo@gmail.com>
2024-09-29 20:03:40 +0200

    Fixed local highscore

--- SpeedJamGame/HighscoreData.gd
+++ SpeedJamGame/HighscoreData.gd
@@ -3,63 +3,66 @@ extends RefCounted
 var max_levels = 5
 var max_scores = 5
 #2d Array, row is levels, column scores
-var levelRecords: Array = []
+var savedHighscores: Dictionary = {}
 
 func _init(max_levels = 5, max_scores = 5):
 	self.max_levels = max_levels
 	self.max_scores = max_scores
+	savedHighscores = {}
 	
-	var inner_array = []
-	for j in range(max_scores):
-		inner_array.append(0)
-
-	for i in range(max_levels):
-		levelRecords.append(inner_array.duplicate())
-
 #Save record and return index position of record
 # -1 if not a record
 func set_record(level:int, newrecord:int) -> int:
-	var index = level-1
-	var records:Array = levelRecords[index] as Array
 	
-	records.sort()
-	var new_record_index = -1
-	for j in range(records.size()):
-		if records[j] < newrecord:
-			new_record_index = j
-			break
-			
-	if new_record_index >= 0:
-		for j in range(new_record_index, records.size()-1):
-			records[j+1] = records[j]
-		records[new_record_index] = newrecord
-		return new_record_index
-		
-	return -1
+	var recordIndex = -1
+	if not savedHighscores.has(str(level)):
+		savedHighscores[str(level)] = [newrecord]
+		recordIndex = 0
+	else:
+		var levelScores:Array = savedHighscores[str(level)].duplicate()
+		for lIndex in range(levelScores.size()):
+			if newrecord < levelScores[lIndex]:
+				recordIndex = lIndex
+				break
+
+		if recordIndex == -1 and levelScores.size() < max_scores:
+			levelScores.append(newrecord)
+			recordIndex = levelScores.size()-1
+		elif recordIndex > -1:
+			if levelScores.size() < max_scores:
+				levelScores += [levelScores[-1]]
+			for index in range(levelScores.size()-1, recordIndex, -1):
+				levelScores[index] = levelScores[index-1]
+			levelScores[recordIndex] = newrecord
+		savedHighscores[str(level)] = levelScores
+	print("Set result: ", savedHighscores, " - New result: ", newrecord, " - index result: ", recordIndex)
+	return recordIndex
 
 func get_records(level:int) -> Dictionary:
-	var index = level-1
 	
-	var array = levelRecords[index]
+	var array:Array = savedHighscores.get(str(level))
+	if not array: array = []
+	
 	var output:Dictionary = {}
 	for j in range(max_scores):
-		output[j+1] = array[j]
+		if j < array.size():
+			output[j+1] = array[j]
+		else:
+			output[j+1] = 0
 		
 	return output
 
-func set_data(raw_data: PackedInt64Array):
-	for lvl in range(max_levels):
-		for score in range(max_scores):
-			if raw_data.size() <= lvl * max_scores + score:
-				break
-			levelRecords[lvl][score] = raw_data[lvl * max_scores + score]
-			
-func convert_data() -> PackedInt64Array:
-	var output = PackedInt64Array()
+func set_data(raw_data: PackedByteArray):
+	var dataString = raw_data.get_string_from_utf8()
+	print("Data string: ", dataString)
+	var result = JSON.parse_string(raw_data.get_string_from_utf8())
+	if (result):
+		savedHighscores = result
+		
+	print("Set data: ", savedHighscores)
 
-	for lvl in range(max_levels):
-		for score in range(max_scores):
-			output.append(levelRecords[lvl][score])
-	return output
-
+func convert_data() -> PackedByteArray:
+	print("Convert data: ", savedHighscores)
+	return JSON.stringify(savedHighscores).to_utf8_buffer()
+	
 
--- SpeedJamGame/HighscoreRecorder.gd
+++ SpeedJamGame/HighscoreRecorder.gd
@@ -5,22 +5,19 @@ var filepath:String
 func _init(filepath: String):
 	self.filepath = filepath
 	
-func read_file() -> PackedInt64Array:
+func read_file() -> PackedByteArray:
 	assert(filepath, "Filepath not set")
 	
 	if not FileAccess.file_exists(filepath):
-		return PackedInt64Array()
+		return PackedByteArray()
 	
 	var fAccess:FileAccess = FileAccess.open(filepath, FileAccess.READ)
 	var inputButter: PackedByteArray = fAccess.get_buffer(fAccess.get_length())
-	fAccess.close()
-	return inputButter.to_int64_array()
+	return inputButter
 
-func write_file(input_data: PackedInt64Array):
+func write_file(input_data: PackedByteArray):
 	var fAccess:FileAccess = FileAccess.open(filepath, FileAccess.WRITE)
-	fAccess.store_buffer(input_data.to_byte_array())
-	print(fAccess.get_error())
+	fAccess.store_buffer(input_data)
 	fAccess.flush()
-	fAccess.close()
 	
 	
